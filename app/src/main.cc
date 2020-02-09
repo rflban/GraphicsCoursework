3d50f8c65782155eba390de70809192ec2928dd1
rflban <nabiev.alsn@yandex.ru>
2020-02-09 15:52:56 +0300

    .

--- app/src/main.cc
+++ app/src/main.cc
@@ -5,18 +5,63 @@
 #include <cstdio>
 #include <cmath>
 
+#include <iostream>
+#include <random>
+#include <map>
+
 int main()
 {
-    Pixmap p(500, 500);
+    Pixmap p(900, 900);
     Painter painter(&p);
 
-    painter.fillRect(0, 0, 500, 500, {255, 255, 255});
+    std::mt19937 gen(time(NULL));
+    std::normal_distribution<> nd(0, 0.015);
+    std::normal_distribution<> nd1(0, 1);
 
-    double angle = 0;
-    for (int a = 5; a <= 200; a += 5)
+    double scale_ratio = 100;
+    double radius = 3.5;
+    double radius_core = 0.25;
+
+    painter.fillRect(0, 0, 900, 900, { 0, 0, 0 });
+    painter.setColor({ 255, 255, 255 });
+
+    double ones[2] = { -1, 1 };
+
+    double basemean = 10;
+    double stddev;
+
+    for (int idx = 0; idx < 1000; idx++)
     {
-        painter.drawEllipse(250, 250, a, a * 0.85, angle);
-        angle += M_PI / 20;
+        double theta = M_PI * nd(gen);
+        double r = radius * ((double)gen() / gen.max()) * ones[gen() % 2];
+
+        if (fabs(r) <= radius_core)
+        {
+            theta = M_PI * nd1(gen);
+        }
+
+        //double mean = basemean * ((fabs(r) / radius));
+        double mean = basemean * (1 - exp((-(radius - fabs(r)) * (radius - fabs(r))) / 1));
+        //double mean = basemean;
+        stddev = mean * 0.5;
+
+        std::normal_distribution<> nd2(mean, stddev / 3);
+        std::cout << mean << ' ' << stddev << ' ' << nd2(gen) << '\n';
+
+        double k = 1;
+
+        if (fabs(r) > radius_core)
+        {
+            k += nd2(gen);
+        }
+
+        int x = round(r * cos(theta) * scale_ratio);
+        //int y = round(r * sin(theta) * scale_ratio * 0.7);
+        int y = round(r * sin(theta) * scale_ratio * 1 + r * sin(theta * 4) * scale_ratio / 1 * (1 - fabs(r) / radius));
+        //int y = round(r * sin(theta) * scale_ratio * 1);
+        //std::cout << mean << ' ' << stddev << ' ' << nd2(gen) << '\n';
+
+        painter.fillPixel(450 + x, 450 + y);
     }
 
     ImageGenerator *generator = new PPMGenerator;
